58477112949ff2070ab1c7c41d7ba7ca857ca4c8
U-실습실\4F <4F@.(none)>
2016-05-02 11:53:30 +0900

    test commit

---
+++ test.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void *c(void* i){
+	printf("Thread %d says hi\n",i);
+	printf("Thread %d says bye\n", i);
+}
+
+int main(int argc, char *argv[]){
+	pthread_t threads[20];
+	int rc, t;
+	for(t=0; t<20; t++){
+		rc = pthread_create(&threads[t],NULL,c,(void*)t);
+		if(rc){
+			printf("ERROR code is %d\n",rc);
+			exit(-1);
+		}
+	}
+	printf("main thread end\n");
+	pthread_exit(NULL);
+}
+
